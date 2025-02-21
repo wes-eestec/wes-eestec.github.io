d12333d93bdef40bf757afd67c98ebb6f77bbd03
Karlo Vukelić <karlo.vukelic@eestec.hr>
2025-02-21 20:15:47 +0100

    seo tweaks

--- src/assets/scripts/header.js
+++ src/assets/scripts/header.js
@@ -16,7 +16,7 @@ hamburgerMenu.addEventListener("click", () => {
 
 // COUNTDOWN https://www.w3schools.com/howto/howto_js_countdown.asp
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 25, 2025 15:37:25").getTime();
+var countDownDate = new Date("Mar 24, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
